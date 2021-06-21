3b4792917279cacbc383cacc26861aacf02d1187
nobishino <nobishii11@gmail.com>
2021-06-21 16:19:15 +0900

    方向づけられたchannel引数にはレポートしないことのテストを追加する

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -3,3 +3,7 @@ package a
 func f(ch chan int) { // want "channel argument should be directed"
 	// The pattern can be written in regular expression.
 }
+
+func g(ch <-chan string) {}
+
+func h(ch chan<- float64) {}
